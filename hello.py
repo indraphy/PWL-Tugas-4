3d2cb9ad3c3a20b86e8922d3bceb21119778804f
Indraphy <indrajack.ij2@gmail.com>
2023-10-22 20:42:14 +0700

    test

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from pyramid.config import Configurator
 from pyramid.view import view_config
 from pyramid.response import Response
 
-@view_config(route_name='hello', renderer='views/loginjj.jinja2')
+@view_config(route_name='hello', renderer='views/login.jinja2')
 def hello(request):
     return {}
 
